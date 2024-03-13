6736393284534c22d36ba8669d8cb5e99ed02de4
UndyingSerpent <undying.serpent@gmail.com>
2024-03-14 00:13:04 +0300

    Change path to file

--- git/create_deployment_etl.py
+++ git/create_deployment_etl.py
@@ -3,7 +3,7 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/UndyingSerpent/prefect.git",
-        entrypoint="etl-dwh-s.py:etl",
+        entrypoint="./git/etl-dwh-s.py:etl",
     ).deploy(
         name="etl-dwh-s",
         work_pool_name="my-managed-pool",
